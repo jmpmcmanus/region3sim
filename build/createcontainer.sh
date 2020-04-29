05656ee466a6c66d7e3eeec07c9845c2325c9dfe
James McManus <jmpmcmanus@adcirc-db.edc.renci.org>
2020-04-29 14:28:23 -0400

    changed image name

--- build/createcontainer.sh
+++ build/createcontainer.sh
@@ -2,5 +2,5 @@ docker run -ti --name region3db_container \
    -p 5434:5432 \
    --volume /projects/regionthree/ingestProcessing:/home/data \
    --volume /projects/regionthree/dockerstorage:/var/local/postgresql \
-  region3db_image_test /bin/bash
+  region3db_image /bin/bash
 
